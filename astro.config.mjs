1ec4bc6f984cd999e12ee255207540734ab661ce
xXErDaniXx06. <danielcarrascogarcia18@gmail.com>
2026-05-25 02:05:05 +0200

    chore: configura @astrojs/node v8 para habilitar Firebase SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'middleware'
+  }),
   integrations: [tailwind()],
 });
