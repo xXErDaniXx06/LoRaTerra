eabc75d25a8284a18558eaaf91b3b985038953bd
xXErDaniXx06. <danielcarrascogarcia18@gmail.com>
2026-05-25 13:31:34 +0200

    chore: elimina directorio .firebase de git y lo añade a gitignore para evitar alertas de API Key

--- .firebase/loraterra-web/functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_CkJ7zC8e.mjs';
--- .firebase/loraterra-web/functions/server.js
+++
@@ -1,4 +0,0 @@
-import { onRequest } from 'firebase-functions/v2/https';
-  const server = import('firebase-frameworks');
-  export const ssrloraterraweb = onRequest({"region":"europe-west1"}, (req, res) => server.then(it => it.handle(req, res)));
-  
